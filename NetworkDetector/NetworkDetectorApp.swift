bb990d1ada44e2d96e78a90285f36a72f0684e0d
tomek <2002tomo@gmail.com>
2024-04-15 10:33:44 +0200

    Initial Commit

---
+++ NetworkDetector/NetworkDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkDetectorApp.swift
+//  NetworkDetector
+//
+//  Created by Tomek Lewiński on 15/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
